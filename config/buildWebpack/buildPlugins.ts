64bbdca367e957be851aa45361b0f6562d2cfd68
Hvostyk <natlitvinandmat@gmail.com>
2026-02-22 01:10:00 +0300

    gitignore inaccurate naming fix

---
+++ config/buildWebpack/buildPlugins.ts
@@ -0,0 +1,27 @@
+import { Configuration, ProgressPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+
+export function buildPlugins(options: BuildOptions): Configuration["plugins"] {
+    const { mode, paths } = options;
+
+    const plugins: Configuration["plugins"] = [new HtmlWebpackPlugin({ template: paths.html })];
+
+    switch (mode) {
+        case "development":
+            plugins.push(new ProgressPlugin());
+            break;
+
+        case "production":
+            plugins.push(
+                new MiniCssExtractPlugin({
+                    filename: "css/[name].[contenthash:8].css",
+                    chunkFilename: "css/[name].[contenthash:8].css",
+                }),
+            );
+            break;
+    }
+
+    return plugins;
+}
