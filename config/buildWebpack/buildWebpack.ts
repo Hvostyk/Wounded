fcedd5dc3b3c03b7d3e46c3caee595cac01fc70d
Hvostyk <natlitvinandmat@gmail.com>
2026-02-22 01:40:20 +0300

    demo bugFix for action succeed

--- config/buildWebpack/buildWebpack.ts
+++ config/buildWebpack/buildWebpack.ts
@@ -1,6 +1,6 @@
 import webpack from "webpack";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import path from "path";
 import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
