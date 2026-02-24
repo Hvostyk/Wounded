f23b9e8d1752311b6f21701640b0ba14e7ee4a7b
Hvostyk <natlitvinandmat@gmail.com>
2026-02-24 19:55:57 +0300

    lint fix

--- tests/src/app/routes.test.ts
+++ tests/src/app/routes.test.ts
@@ -1,9 +1,8 @@
 import { getNavigationRoutes } from "../../../src/app/routes";
 
 describe("Routes tests", () => {
-
     test("getNavgationRoutes should return correct number of paths", () => {
         const navgationRoutes = getNavigationRoutes();
-        expect(navgationRoutes.length).toEqual(3)
-    })
-})
+        expect(navgationRoutes.length).toEqual(3);
+    });
+});
